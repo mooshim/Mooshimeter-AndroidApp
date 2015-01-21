aeb0448bf85774145ca10fb1b73dd3bc025315e0
James Whong <james.whong@gmail.com>
2015-01-21 14:08:54 -0800

    Vastly improved graph formatting, second Y axis, colors.

--- app/src/main/java/com/mooshim/mooshimeter/main/TrendActivity.java
+++ app/src/main/java/com/mooshim/mooshimeter/main/TrendActivity.java
@@ -1,6 +1,7 @@
 package com.mooshim.mooshimeter.main;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,9 @@ import android.view.MenuItem;
 import android.view.OrientationEventListener;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.SecondScale;
+import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.mooshim.mooshimeter.R;
@@ -22,7 +26,7 @@ public class TrendActivity extends Activity {
     private OrientationEventListener orientation_listener;
     private MooshimeterDevice mMeter;
     private GraphView mGraph;
-    private final LineGraphSeries[] dataPoints = new LineGraphSeries[2];
+    private final LineGraphSeries[] dataSeries = new LineGraphSeries[2];
     private double start_time;
     private boolean cleaning_up = false;
 
@@ -81,13 +85,7 @@ public class TrendActivity extends Activity {
         // Log.d(TAG, "onResume");
         super.onResume();
 
-        mGraph.removeAllSeries();
-        dataPoints[0] = new LineGraphSeries();
-        dataPoints[1] = new LineGraphSeries();
-        mGraph.addSeries(dataPoints[0]);
-        mGraph.addSeries(dataPoints[1]);
-
-        start_time = milliTime();
+        getActionBar().hide();
 
         mMeter = MooshimeterDevice.getInstance();
         if(mMeter == null) {
@@ -100,6 +98,36 @@ public class TrendActivity extends Activity {
         } else {
             onMeterInitialized();
         }
+
+        mGraph.removeAllSeries();
+        mGraph.getViewport().setXAxisBoundsManual(true);
+        mGraph.getViewport().setYAxisBoundsManual(true);
+        final SecondScale ss = mGraph.getSecondScale();
+
+        mGraph.setKeepScreenOn(true);
+        mGraph.setBackgroundColor(Color.BLACK);
+        GridLabelRenderer r = mGraph.getGridLabelRenderer();
+        r.setHorizontalAxisTitle("Time");
+        r.setHorizontalAxisTitleColor(Color.WHITE);
+        r.setHorizontalLabelsColor(Color.WHITE);
+        r.setGridStyle(GridLabelRenderer.GridStyle.BOTH);
+        r.setVerticalAxisTitle(mMeter.getDescriptor(0));
+        r.setVerticalAxisTitleColor(Color.RED);
+        r.setVerticalLabelsColor(Color.RED);
+        // TODO: Second scale needs a title but I don't see a way to set it through GraphView API
+        r.setVerticalLabelsSecondScaleColor(Color.GREEN);
+
+        dataSeries[0] = new LineGraphSeries();
+        dataSeries[1] = new LineGraphSeries();
+        dataSeries[0].setColor(Color.RED);
+        dataSeries[0].setThickness(5);
+        dataSeries[1].setColor(Color.GREEN);
+        dataSeries[1].setThickness(5);
+        mGraph.addSeries(dataSeries[0]);
+        ss.addSeries(dataSeries[1]);
+
+        start_time = milliTime();
+
         orientation_listener.enable();
     }
 
@@ -128,10 +156,36 @@ public class TrendActivity extends Activity {
                         //else   { lsb_int = mMeter.meter_sample.reading_lsb[c]; }
                         lsb_int = mMeter.meter_sample.reading_lsb[0];
                         val = mMeter.lsbToNativeUnits(lsb_int, 0);
-                        dataPoints[0].appendData(new DataPoint(new_time,val),true,100);
+                        dataSeries[0].appendData(new DataPoint(new_time,val),false,500);
                         lsb_int = mMeter.meter_sample.reading_lsb[1];
                         val = mMeter.lsbToNativeUnits(lsb_int, 1);
-                        dataPoints[1].appendData(new DataPoint(new_time,val),true,100);
+                        dataSeries[1].appendData(new DataPoint(new_time,val),false,500);
+
+                        final Viewport vp = mGraph.getViewport();
+
+                        // Manually reset axis boundaries
+                        final double padding_factor = 0.1;
+                        final double min_x = dataSeries[0].getLowestValueX();
+                        final double max_x = dataSeries[0].getHighestValueX();
+                        final double x_span = max_x - min_x;
+
+                        // MaxX must be padded to leave room for the second Y axis
+                        vp.setMinX(min_x);
+                        vp.setMaxX(max_x-padding_factor*x_span);
+
+                        final double min_y1 = dataSeries[0].getLowestValueY();
+                        final double max_y1 = dataSeries[0].getHighestValueY();
+                        final double y1_span = max_y1 - min_y1;
+
+                        vp.setMinY(min_y1 - padding_factor*y1_span);
+                        vp.setMaxY(max_y1 + padding_factor*y1_span);
+
+                        final double min_y2 = dataSeries[1].getLowestValueY();
+                        final double max_y2 = dataSeries[1].getHighestValueY();
+                        final double y2_span = max_y2 - min_y2;
+
+                        mGraph.getSecondScale().setMinY(min_y2 - padding_factor*y2_span);
+                        mGraph.getSecondScale().setMaxY(max_y2 + padding_factor*y2_span);
                     }
                 });
             }
@@ -142,6 +196,7 @@ public class TrendActivity extends Activity {
     protected void onPause() {
         // Log.d(TAG, "onPause");
         super.onPause();
+        getActionBar().show();
         mMeter.meter_settings.target_meter_state = MooshimeterDevice.METER_PAUSED;
         mMeter.sendMeterSettings(new Block() {
             @Override
