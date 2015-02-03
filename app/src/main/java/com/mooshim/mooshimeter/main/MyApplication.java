d3257e9f2473886775c72d6a33228854407e30aa
James Whong <james.whong@gmail.com>
2015-02-03 02:08:10 -0800

    Added crash reporting.  Also installed necessary scripts on the webserver to collect the reports.

---
+++ app/src/main/java/com/mooshim/mooshimeter/main/MyApplication.java
@@ -0,0 +1,31 @@
+package com.mooshim.mooshimeter.main;
+
+/**
+ * Created by First on 2/3/2015.
+ */
+
+import android.app.Application;
+
+import com.mooshim.mooshimeter.R;
+
+import org.acra.*;
+import org.acra.annotation.*;
+
+@ReportsCrashes(
+        formKey = "", // will not be used
+        formUri = "https://moosh.im/crash/report.php",
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast_text
+)
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
+}
+
+
+
