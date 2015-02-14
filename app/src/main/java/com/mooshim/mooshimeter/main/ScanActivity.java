742fedba81a8316aad2ffc89eb5d321d9db80718
James Whong <james.whong@gmail.com>
2015-02-13 17:41:24 -0800

    Copyrights

--- app/src/main/java/com/mooshim/mooshimeter/main/ScanActivity.java
+++ app/src/main/java/com/mooshim/mooshimeter/main/ScanActivity.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) Mooshim Engineering LLC 2015.
+ *
+ * This file is part of Mooshimeter-AndroidApp.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Mooshimeter-AndroidApp.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package com.mooshim.mooshimeter.main;
 
 import android.bluetooth.BluetoothAdapter;
@@ -32,9 +51,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
 
-/**
- * Created by First on 2/4/2015.
- */
 public class ScanActivity extends FragmentActivity {
     // Defines
     private static String TAG = "ScanActivity";
