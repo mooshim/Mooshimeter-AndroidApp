cccedae2ca6950c0907ec027d2460d8ba08f5577
James Whong <james.whong@gmail.com>
2016-04-15 14:31:42 -0700

    Adds todos

--- app/src/main/java/com/mooshim/mooshimeter/common/ThermocoupleHelper.java
+++ app/src/main/java/com/mooshim/mooshimeter/common/ThermocoupleHelper.java
@@ -34,7 +34,7 @@ public class ThermocoupleHelper {
             return out;
         }
     }
-    public static final helper J = new helper(new double[]{
+    public static final helper J = new helper(new double[]{ // TODO: ADD TERMS
             0.0,
             1.978425e-2,
             -2.001204e-7,
@@ -53,35 +53,35 @@ public class ThermocoupleHelper {
             1.057734e-30,
             -1.052755e-35,
     },0,500);
-    public static final helper T = new helper(new double[]{
+    public static final helper T = new helper(new double[]{ // TODO: ADD TERMS
             0.0,
             2.592800e-2,
             -7.602961e-7,
             4.637791e-11,
             -2.165394e-15,
     },0,400);
-    public static final helper E = new helper(new double[]{
+    public static final helper E = new helper(new double[]{ // TODO: ADD TERMS
             0.0,
             1.7057035e-2,
             -2.3301759e-7,
             6.5435585e-12,
             -7.3562749e-17,
     },0,1000);
-    public static final helper N = new helper(new double[]{
+    public static final helper N = new helper(new double[]{ // TODO: ADD TERMS
             0.0,
             3.8783277e-2,
             -1.1612344e-6,
             6.9525655e-11,
             -3.0090077e-15,
     },0,600);
-    public static final helper B = new helper(new double[]{
+    public static final helper B = new helper(new double[]{ // TODO: ADD TERMS
             9.842332e1,
             6.997150e-1,
             -8.4765304e-4,
             1.0052644e-6,
             -83345952e-10,
     },250,700);
-    public static final helper R = new helper(new double[]{
+    public static final helper R = new helper(new double[]{ // TODO: ADD TERMS
             0.0,
             1.8891380e-1,
             -9.3835290e-5,
@@ -89,7 +89,7 @@ public class ThermocoupleHelper {
             -2.2703580e-10,
             3.5145659e-13,
     },-50,250);
-    public static final helper S = new helper(new double[]{
+    public static final helper S = new helper(new double[]{ // TODO: ADD TERMS
             0.0,
             1.84949460e-1,
             -8.00504062e-5,
