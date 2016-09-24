a9ace48743c4abf6abedcaef85a5e830335935ee
Albin Eldstål-Damlin <laeder.keps@gmail.com>
2016-09-24 22:31:11 +0200

    Fixed start and end crackle for continuous meter beep
    The AudioTrack class from the Android API doesn't expose a very
    useful interface (no way to query buffer status, no way to change
    looping parameters without stopping playback), so for now we emulate
    a PCM stream and use callbacks to know when to refill the buffer.

--- app/src/main/java/com/mooshim/mooshimeter/common/Alerter.java
+++ app/src/main/java/com/mooshim/mooshimeter/common/Alerter.java
@@ -15,12 +15,21 @@ import java.util.concurrent.TimeUnit;
  * Created by First on 6/5/2016.
  */
 public class Alerter implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static int bufferSize;
     private static final int duration = 1; // seconds
     private static final int sampleRate = 8000;
-    private static final int leadin = 200;
-    private static final int numSamples = duration * sampleRate;
-    private static final double freqOfTone = 880; // hz
-    private static final int samplesPerLoop = (int)(100 * sampleRate/freqOfTone);
+    private static final double freqOfTone = 800; // hz
+    private static final int samplesPerPeriod = (int)(sampleRate/freqOfTone);
+    private static final short leadin = 10 * samplesPerPeriod;
+    private static final short body = 300 * samplesPerPeriod;
+    private static final short leadout = 10 * samplesPerPeriod;
+    private static final int numSamples = leadin + body + leadout;
+
+    // Generated PCM samples for the lead-in, some number of full periods and then lead-out.
+    // Playing all three back to back should give a jitter- and crackle-free tone.
+    private static short[] sampHead = new short[leadin];
+    private static short[] sampBody = new short[body];
+    private static short[] sampTail = new short[leadout];
 
     private static final long[] vibratePattern = { 0, 50, 200, 50,};    // Pattern for Vibrator.vibrate()
 
@@ -29,38 +38,56 @@ public class Alerter implements SharedPreferences.OnSharedPreferenceChangeListen
     private static AudioTrack audioTrack;
     private static Vibrator vibrator;
     private static boolean currentlyVibrating = false;
+    private static boolean currentlyBeeping = false;
 
     private static ScheduledThreadPoolExecutor executor;
     private static Runnable stopAlert;
     private static ScheduledFuture nextStopAlert;
 
+    private static short waveform(short i) {
+        return (short) (0.5 * Math.sin(2 * Math.PI * i / (samplesPerPeriod)) * 32767);
+    }
+
     private static AudioTrack generateTone() {
 
-        final float sample[] = new float[numSamples];
-        // fill out the array
-        for (int i = 0; i < numSamples; ++i) {
-            sample[i]= (float)Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+        // Attenuated intro
+        for (short i = 0; i < leadin; ++i) {
+            float frac = (float) i / leadin;
+            sampHead[i] = (short) (waveform(i) * frac);
         }
-        // Put an envelope on the beginning and end to avoid speaker crackle
-        for(int i = 0; i < leadin; i++) {
-            float frac = ((float)i)/leadin;
-            sample[i] *= frac;
-            sample[sample.length-(i+1)] *= frac;
+
+        // Full-volume waveform in between
+        for (short i = 0; i < body; ++i) {
+            sampBody[i] = waveform(i);
         }
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
-        byte generatedSnd[] = new byte[numSamples*2];
-        int idx = 0;
-        for (double dVal : sample) {
-            short val = (short) (dVal * 32767);
-            generatedSnd[idx++] = (byte) (val & 0x00ff);
-            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        // Attenuated outro
+        for (short i = 0; i < leadout; ++i) {
+            float frac = (float) 1 -  (float)i/ leadout;
+            sampTail[i] = (short) (waveform(i) * frac);
         }
+
+        // Since AudioTrack.write() is blocking, we need to have enough
+        // buffer space to write each new chunk Rate/freqOfToneof audio while there is still
+        // enough old data in the buffer to play without a hickup.
         AudioTrack ret = new AudioTrack(AudioManager.STREAM_RING,
-                8000, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
-                AudioTrack.MODE_STATIC);
-        ret.write(generatedSnd, 0, generatedSnd.length);
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, 2 * numSamples,
+                AudioTrack.MODE_STREAM);
+
+        ret.setPlaybackPositionUpdateListener(new AudioTrack.OnPlaybackPositionUpdateListener() {
+            @Override
+            public void onMarkerReached(AudioTrack track) {
+                // Do nothing, we don't have any markers
+            }
+
+            @Override
+            public void onPeriodicNotification(AudioTrack track) {
+                if (currentlyBeeping) {
+                    track.write(sampBody, 0, sampBody.length);
+                }
+            }
+        });
 
         return ret;
     }
@@ -81,23 +108,44 @@ public class Alerter implements SharedPreferences.OnSharedPreferenceChangeListen
                 stopAlerting();
             }
         };
+
+        audioTrack.play();
     }
 
     private static void beep() {
         if(audioTrack.getPlayState() != AudioTrack.PLAYSTATE_PLAYING) {
 
             // On the initial play, we play including the attenuated lead-in
-            // After that, we loop full periods of beep to give a continuous tone.
-            audioTrack.stop();
-            audioTrack.setLoopPoints(leadin, leadin + samplesPerLoop, -1);
+            // After that, we periodically feed in full periods of wave to get a continuous beep.
+            audioTrack.flush();
+            currentlyBeeping = true;
+
+            // Call back every time this many frames have been played, so we know we can
+            // enqueue another bit of audio data.
+            audioTrack.setPositionNotificationPeriod(body);
+
             audioTrack.setPlaybackHeadPosition(0);
+            audioTrack.write(sampHead, 0, sampHead.length);
+
+            // Playback will not start if we haven't filled up minBuffer bytes.
+            // The exact number is device-dependent!
+            // Prime the buffer with enough full waveform periods.
+            audioTrack.write(sampBody, 0, sampBody.length);
+            audioTrack.write(sampBody, 0, sampBody.length);
+
             audioTrack.play();
         }
-
 
     }
 
     private static void stopBeeping() {
+        if (currentlyBeeping) {
+            // Let the buffer deplete after we've played the lead-out
+            audioTrack.write(sampTail, 0, sampTail.length);
+            currentlyBeeping = false;
+        }
+
+        // Finish playing what's been buffered and then stop
         audioTrack.stop();
     }
 
